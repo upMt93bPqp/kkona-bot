bee661df8936ec1543625ba619f01891e92a962e
upMt93bPqp <37460878+upMt93bPqp@users.noreply.github.com>
2018-03-17 03:06:55 +0200

    Update kkona.js

--- kkona.js
+++ kkona.js
@@ -1,7 +1,7 @@
 const Asetukset = require("./cfg/asetukset.json");
 const Discord = require("discord.js");
-const bot = new Discord.Client();
 const fs = require("fs");
+const bot = new Discord.Client();
 
 bot.commands = new Discord.Collection();
 
